daa1dcdb133814c6c45f184b39fc22cf5848d028
amazing-hashmap-2071 <haim11293@aol.com>
2019-04-28 14:53:59 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,5 +16,19 @@ let index = 0;
 
 function init() {
   // your code here
+    window.addEventListener("keydown", function(event){
+    const key = parseInt(event.which);
+    console.log(event.which);
+    if(key === codes[index]){
+      index++;
+      if(index === codes.length){
+        window.alert("You found the Konami Code!")
+      }
+    } else{
+      index = 0;
+    }
+  })
+}
+init();
 }
 
